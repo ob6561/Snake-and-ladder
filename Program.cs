ea2aa5e71aab44722b4a971d48e1cc227526dd96
Om_Bandyopadhyay <ombandyopadhyay@gmail.com>
2025-11-06 12:01:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Snake_and_ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
