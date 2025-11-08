497802a2d098f537328c7bc064da90caaa8c3b68
Om_Bandyopadhyay <ombandyopadhyay@gmail.com>
2025-11-08 12:19:33 +0530

    UC3: add random dice roll and update position

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@
         {
             Console.WriteLine("snake ladder game");
             int pos = 0;
+            var rnd = new Random();
+            int dice = rnd.Next(1, 7);
+            Console.WriteLine("Player rolled dice: " + dice);
+            pos += dice;
             Console.WriteLine("Starting position of the player is: " + pos);
         }
     }
