f412aa55f35a1c9ab28a70cf4cd88640ac9d0dce
Om_Bandyopadhyay <ombandyopadhyay@gmail.com>
2025-11-08 12:14:32 +0530

    Start UC3 from UC2 Program.cs baseline (pos = 0)

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
-﻿using System;
-namespace Snake_and_ladder
+﻿namespace Snake_and_ladder
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-
-
+            Console.WriteLine("snake ladder game");
+            int pos = 0;
+            Console.WriteLine("Starting position of the player is: " + pos);
         }
     }
 }
